f70e1e7258721537bdea2e46bdc53e86f13f035b
MSI <kulakov056@mail.ru>
2023-11-26 22:16:11 +0500

    create branch and finished doing home work

--- src/main/java/pro/sky/calculator/controller/CalculatorController.java
+++ src/main/java/pro/sky/calculator/controller/CalculatorController.java
@@ -10,41 +10,41 @@ public class CalculatorController {
 
     private final CalculatorService calculatorService;
 
-    public CalculatorController(CalculatorService calculatorService){
+    public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
 
     @GetMapping
-    public String welcome(){
-        return CalculatorService.welcome();
+    public String welcome() {
+        return calculatorService.welcome();
     }
 
     @GetMapping("/plus")
-    public String plus(@RequestParam Integer a, @RequestParam Integer b){
-    Integer result = calculatorService.plus(a,b);
-    return a + " + " + b + " = " + result;
+    public String plus(@RequestParam Integer a, @RequestParam Integer b) {
+        Integer result = calculatorService.plus(a, b);
+        return a + " + " + b + " = " + result;
     }
 
     @GetMapping("/minus")
-    public String minus(@RequestParam Integer a, @RequestParam Integer b){
-        Integer result = calculatorService.minus(a,b);
+    public String minus(@RequestParam Integer a, @RequestParam Integer b) {
+        Integer result = calculatorService.minus(a, b);
         return a + " - " + b + " = " + result;
     }
 
     @GetMapping("/multiply")
-    public String multiply(@RequestParam Integer a, @RequestParam Integer b){
-        Integer result = calculatorService.multiply(a,b);
+    public String multiply(@RequestParam Integer a, @RequestParam Integer b) {
+        Integer result = calculatorService.multiply(a, b);
         return a + " * " + b + " + " + " = " + result;
     }
 
     @GetMapping("/divide")
-    public String divide(@RequestParam Integer a, @RequestParam Integer b){
-        Double result = calculatorService.divide(a,b);
+    public String divide(@RequestParam Integer a, @RequestParam Integer b) {
+        Double result = calculatorService.divide(a, b);
         return a + " / " + b + " = " + result;
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public String divideByZero(IllegalArgumentException e, HttpServletResponse response){
+    public String divideByZero(IllegalArgumentException e, HttpServletResponse response) {
         response.setStatus(400);
         return e.getMessage();
     }
--- src/test/java/pro/sky/calculator/service/CalculatorServiceImplTest.java
+++ src/test/java/pro/sky/calculator/service/CalculatorServiceImplTest.java
@@ -1,6 +1,8 @@
 package pro.sky.calculator.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,16 +10,23 @@ class CalculatorServiceImplTest {
 
     private final CalculatorService calculatorService = new CalculatorServiceImpl();
     private final int EXPECTED = 15;
+
     @Test
     void welcome() {
-    String expected = "Welcome to Calculator";
-    assertEquals(expected,CalculatorService.welcome());
+        String expected = "Welcome to Calculator";
+        assertEquals(expected, calculatorService.welcome());
     }
 
     @Test
     void plus() {
-    int expected = 15;
-    assertEquals(expected,calculatorService.plus(5,10));
+        int expected = 15;
+        assertEquals(expected, calculatorService.plus(5, 10));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3", "5,10,15", "-10,15,5", "0,0,0", "0,-1,0"})
+    void plusPasitive(Integer a, Integer b, Integer expected) {
+        assertEquals(expected, calculatorService.plus(a, b));
     }
 
     @Test
@@ -29,6 +38,20 @@ class CalculatorServiceImplTest {
     }
 
     @Test
-    void divide() {
+    void dividePositive() {
+        double expected = 10;
+        calculatorService.plus(null, null);
+        assertEquals(expected, calculatorService.divide(5, 5));
     }
+
+    @Test
+    void divideByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calculatorService.divide(Integer.MAX_VALUE, 0));
+    }
+
+    @Test
+    void divideByNull() {
+        assertThrows(NullPointerException.class, () -> calculatorService.divide(Integer.MAX_VALUE, null));
+    }
+
 }
